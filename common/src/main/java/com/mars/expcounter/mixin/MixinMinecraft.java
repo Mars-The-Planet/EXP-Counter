b2fac13b5761495b3d8d526519538de538fcc7cb
Mars <ruzicka.maty@gmail.com>
2025-06-19 10:48:46 +0200

    fixed crashing in 1.21.6

--- common/src/main/java/com/mars/expcounter/mixin/MixinMinecraft.java
+++ common/src/main/java/com/mars/expcounter/mixin/MixinMinecraft.java
@@ -1,11 +1,13 @@
 package com.mars.expcounter.mixin;
 
-import com.mars.expcounter.Constants;
 import com.mars.expcounter.ExpCounterConfig;
+import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
-import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.contextualbar.ContextualBarRenderer;
+import net.minecraft.client.gui.contextualbar.ExperienceBarRenderer;
+import net.minecraft.client.player.LocalPlayer;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -14,28 +16,29 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Gui.class)
-public abstract class MixinMinecraft {
-    @Shadow
-    @Final
-    protected Minecraft minecraft;
+@Mixin(ExperienceBarRenderer.class)
+public abstract class MixinMinecraft implements ContextualBarRenderer {
+    @Shadow @Final private Minecraft minecraft;
 
-    @Shadow public abstract Font getFont();
+    @Inject(method = "renderBackground", at = @At("RETURN"))
+    public void renderExperienceBar(GuiGraphics guiGraphics, DeltaTracker deltaTracker, CallbackInfo ci) {
+        LocalPlayer localPlayer = this.minecraft.player;
+        Font font = this.minecraft.font;
 
-    @Inject(method = "renderExperienceBar", at = @At("RETURN"))
-    public void renderExperienceBar(GuiGraphics guiGraphics, int p_282731_, CallbackInfo ci) {
         if(ExpCounterConfig.show_next_level){
-            int currentExp = (int) (this.minecraft.player.getXpNeededForNextLevel() * this.minecraft.player.experienceProgress);
-            String nextLvlString = currentExp + "/" + this.minecraft.player.getXpNeededForNextLevel();
-            int xNextLevel = ((guiGraphics.guiWidth() - this.getFont().width(nextLvlString)) / 2) + ExpCounterConfig.position_off_centre_next_level;
+            int currentExp = (int) (localPlayer.getXpNeededForNextLevel() * localPlayer.experienceProgress);
+            String nextLvlString = currentExp + "/" + localPlayer.getXpNeededForNextLevel();
+            int xNextLevel = ((guiGraphics.guiWidth() - font.width(nextLvlString)) / 2) + ExpCounterConfig.position_off_centre_next_level;
             int yNextLevel = guiGraphics.guiHeight() - ExpCounterConfig.position_y_next_level;
-            guiGraphics.drawString(this.getFont(), nextLvlString, xNextLevel, yNextLevel, ExpCounterConfig.color_next_level, true);
+            int colNextLevel = 0xFF000000 | (ExpCounterConfig.color_next_level & 0x00FFFFFF);;
+            guiGraphics.drawString(font, nextLvlString, xNextLevel, yNextLevel, colNextLevel, true);
         }
         if(ExpCounterConfig.show_total){
-            String totalExperience = String.valueOf(getRealTotalExperience(this.minecraft.player.experienceLevel, (int) (this.minecraft.player.getXpNeededForNextLevel() * this.minecraft.player.experienceProgress)));
-            int xTotal = ((guiGraphics.guiWidth() - this.getFont().width(totalExperience)) / 2) + ExpCounterConfig.position_off_centre_total;
+            String totalExperience = String.valueOf(getRealTotalExperience(localPlayer.experienceLevel, (int) (localPlayer.getXpNeededForNextLevel() * localPlayer.experienceProgress)));
+            int xTotal = ((guiGraphics.guiWidth() - font.width(totalExperience)) / 2) + ExpCounterConfig.position_off_centre_total;
             int yTotal = guiGraphics.guiHeight() - ExpCounterConfig.position_y_total;
-            guiGraphics.drawString(this.getFont(), totalExperience, xTotal, yTotal, ExpCounterConfig.color_total, true);
+            int colTotal = 0xFF000000 | (ExpCounterConfig.color_total & 0x00FFFFFF);
+            guiGraphics.drawString(font, totalExperience, xTotal, yTotal, colTotal, true);
         }
     }
 
