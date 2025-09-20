60ca3955099ddb6ee1b1bd32a42fabc57c90c0ae
Mars <ruzicka.maty@gmail.com>
2025-09-20 13:15:48 +0200

    fixed min x value

--- common/src/main/java/com/mars/expcounter/ExpCounterConfig.java
+++ common/src/main/java/com/mars/expcounter/ExpCounterConfig.java
@@ -7,9 +7,9 @@ public class ExpCounterConfig extends DeimosConfig {
     @Entry public static boolean show_total = true;
     @Entry public static int color_next_level = 16776960;
     @Entry public static int color_total = 16776960;
-    @Entry public static int position_off_centre_next_level = -50;
+    @Entry(min = -1000) public static int position_off_centre_next_level = -50;
     @Entry public static int position_y_next_level = 30;
-    @Entry public static int position_off_centre_total = 50;
+    @Entry(min = -1000) public static int position_off_centre_total = 50;
     @Entry public static int position_y_total = 30;
 
 }
